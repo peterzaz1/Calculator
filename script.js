5af0b07be218a998c3e0684ebeb613f3e8fa4e3e
Peter <tttyyywa@gmail.com>
2023-08-29 17:24:49 -0700

    added eventlistener to get the data-set from the buttons

--- script.js
+++ script.js
@@ -13,13 +13,21 @@ function operate(firstNumber, lastNumber, operator) {
             return divide(firstNumber, lastNumber);
     }
 }
-let buttons = document.querySelectorAll(".small-b");
-console.log(buttons);
 
 let add = (n1, n2) => n1 + n2;
 let subtract = (n1, n2) => n1 - n2;
 let multiply = (n1, n2) => n1 * n2;
 let divide = (n1, n2) => n1 / n2;
+
+let buttons = document.querySelectorAll(".small-b");
+let disply = document.querySelector(".disply h1");
+console.log(disply);
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        disply.textContent += button.dataset.num;
+    });
+});
 
 let firstNumber = 0;
 let lastNumber = 0;
